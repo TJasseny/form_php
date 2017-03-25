cc745f7654651d76d8671b5d176633d2d44046d5
TJasseny <thibaud.jasseny@hotmail.fr>
2017-03-25 09:59:22 +0100

    création du fichier hello.php

---
+++ hello.php
@@ -0,0 +1,13 @@
+
+<doctype html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Hello world</title>
+<head>
+
+<body>
+<h1>Hello world !</h1>
+<p>Aujourd'hui, nous sommes le <?php echo date('d/m/Y h:i:s'); ?>.</p>
+</body>
+</html>
